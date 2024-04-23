5ea4d207cb8f30320e829bf56b7bbc70ec78a446
deadman00069 <konarkshivam1997@gmail.com>
2024-04-24 02:20:25 +0530

    init 8

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import connectDB from "./database/connect_db.ts";
 import AuthRouter from "./routes/auth.route.ts";
-import TaskRouter from "./routes/task.route.ts";
 import cookieParser from "cookie-parser";
 import ErrorMiddleware from "./middleware/errorMiddleware.ts";
 import { refreshToken } from "./controller/auth.controller.ts";
@@ -15,7 +14,6 @@ app.use(cookieParser());
 
 // Routes
 app.use("/api/v1/auth", AuthRouter);
-// app.use("/api/v1/task", TaskRouter);
 
 app.use(ErrorMiddleware);
 app.listen(port, () => {
